b4c45d4015ed3c36057134624c46fc71c0dada8b
a.shiryaev <a.shiryaev@siberian.capital>
2023-08-22 19:39:47 +0400

    yaml config test

--- test.sh
+++ test.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
+set -e 
+set -o pipefail 
+
+URL="https://raw.githubusercontent.com/lybavsky/vfort/main"
 
 WINURL="https://software.download.prss.microsoft.com/dbazure/Win10_22H2_Russian_x64v1.iso?t=c6925b7f-4981-424a-b2ac-2a1b2835b05b&e=1692794388&h=d1e8e5b5aac2b4ec885ac52c61cba5c4ab6dc4bd96d42c43444807e50a579d9c"
+
+
+catch() {
+  echo "Got some error on line $LINENO"
+  exit 0
+}
+
+trap "catch" ERR
+
 
 function err() {
 	shift
@@ -17,9 +30,21 @@ sed -i /etc/systemd/logind.conf -e '/HandleLidSwitch/d'
 sed -i /etc/systemd/logind.conf -e '$a/HandleLidSwitch=ignore'
 systemctl restart systemd-logind.service
 
-echo "Start to install virtualbox"
+echo "Start to install virtualbox and additional soft"
 apt update 
-apt install -y bridge-utils xinit virtualbox virtualbox-ext-pack virtualbox-guest-utils virtualbox-guest-additions-iso
+apt install -y bridge-utils xinit virtualbox virtualbox-ext-pack virtualbox-guest-utils virtualbox-guest-additions-iso dialog python3 python3-pip
+
+pip3 install yq
 
 echo "Download windows 10 iso"
 wget -O /win.iso "$WINURL"
+
+
+echo "Make VM directories"
+mkdir -p /srv/vm
+
+CFG_URL="$URL/vm.yaml"
+
+curl -f $CFG_URL 2>/dev/null | yq -r '.|to_entries()' | while read jcfg; do
+	echo "Got $jcfg";
+done
