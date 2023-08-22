f7ca014551cf4ca20af711615679ba45199e6ac9
a.shiryaev <a.shiryaev@siberian.capital>
2023-08-22 16:19:59 +0400

    check for root

--- test.sh
+++ test.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-echo test
+function err() {
+	shift
+	msg="$@"
+	echo "Error: $msg"
+	exit 1
+}
+
+[ "$EUID" -ne 0 ] && err "Script sould be started as root"
 
 
+echo "Disable powersave mode when closing laptop"
+
+mkdir -p /etc/NetworkManager/conf.d/
+echo "[connection]
+wifi.powersave = 2" > /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+
+systemctl restart NetworkManager
