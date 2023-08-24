dc51e539da43c2893cd0fcc39268e1177896d809
a.shiryaev <a.shiryaev@siberian.capital>
2023-08-24 17:54:42 +0400

    fix

--- test.sh
+++ test.sh
@@ -2,8 +2,6 @@
 set -e 
 set -o pipefail 
 
-URL="https://raw.githubusercontent.com/lybavsky/vfort/develop"
-
 WINURL="https://software.download.prss.microsoft.com/dbazure/Win10_22H2_Russian_x64v1.iso?t=c6925b7f-4981-424a-b2ac-2a1b2835b05b&e=1692794388&h=d1e8e5b5aac2b4ec885ac52c61cba5c4ab6dc4bd96d42c43444807e50a579d9c"
 WDIR="/srv/vm"
 
@@ -114,9 +112,7 @@ systemctl restart virtualbox
 echo "Make VM directory $WDIR"
 mkdir -p $WDIR
 
-CFG_URL="$URL/vm.yaml"
-
-curl -f $CFG_URL 2>/dev/null | yq -c '.vms|to_entries[]' | while read jcfg; do
+cat "`dirname $( readlink -f $0 )`/vm.yaml" | yq -c '.vms|to_entries[]' | while read jcfg; do
 	echo "Got $jcfg";
 
 
