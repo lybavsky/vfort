23497981a5434f1a0d6492a2de7f80e51358eef4
a.shiryaev <a.shiryaev@siberian.capital>
2023-08-25 16:44:04 +0400

    fix

--- delete.sh
+++ delete.sh
@@ -7,6 +7,30 @@ CDIR="`dirname $( readlink -f $0 )`"
 source ${CDIR}/vars.sh
 source ${CDIR}/functions.sh
 
+items=()
+
+for vm in $WDIR/*; do
+  items+=(${vm##*/} $vm)
+done
+
+exec 4>&1
+selection="$(dialog \
+  --title "Delete VM" \
+  --clear \
+  --cancel-label "Exit" \
+  --menu "Please select VM to delete:" 0 0 4 \
+  ${items[@]} \
+  2>&1 1>&4
+)"
+
+exec 4>&-
+
+if [ -z "$selection"]; then
+  dialog --title "No selection, will exit"
+  exit 0
+fi
+
+echo "selected $selection"
 
 	# TODO: path to get enabled systemd services
 	# /etc/systemd/system/getty.target.wants/win@common.service
