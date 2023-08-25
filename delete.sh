c79b220e7a83ebc89bf3d10768df726c5b9b7c02
a.shiryaev <a.shiryaev@siberian.capital>
2023-08-25 17:58:31 +0400

    fix

--- delete.sh
+++ delete.sh
@@ -40,7 +40,7 @@ while :; do
     vmstate="$(vm_state $vm_name)"
     if [ "$vmstate" != "powered off" -a "$vmstate" != "aborted" ]; then
     	echo "VM state is $vmstate, need to poweroff"
-    	systemctl stop ${UNITNAME}@${vm_name}
+    	systemctl stop ${UNITNAME}@${vm_name}.service
     	vboxmanage controlvm $vm_name poweroff
     fi
     
@@ -51,32 +51,39 @@ while :; do
     hostif="$( vboxmanage showvminfo $vm_name | sed -e '/^NIC 1/!d;s/^.*'"'"'\(.*\)'"'"'.*$/\1/g;s/^.*\s\{1,\}//g' )"
     inlist="$( vboxmanage list hostonlyifs | awk '/^Name/{print $2}' | grep $hostif -q; echo $? )"
     
+
     vbox_net="$( vbox_show hostonlyifs $hostif | awk '/VBoxNetworkName/{ print $2 }' )"
     vbox_net_dhcp="$( vbox_show dhcpservers $vbox_net NetworkName)"
     
+    echo "Checking if enabled dhcpserver for net"
     if [ ! -z "$vbox_net_dhcp" ]; then
       echo "Need to disable dhcpserver for net"
       vboxmanage dhcpserver remove --network="$vbox_net"
     fi
     
+    echo "Checking if hostif available"
     if [ ! -z "$hostif" -a "$inlist" -eq 0 ]; then
       echo "Need to delete hostif $hostif"
       vboxmanage hostonlyif remove $hostif
     fi
-    
-    echo "Will delete VM"
-    vboxmanage unregistervm $vm_name --delete
     
     if [ -f "/srv/vm/$vm_name/disk.vmdk" ]; then
       echo "Probably disk is raw, will delete part"
       part="$( cat "/srv/vm/$vm_name/disk.vmdk" | awk '/RW/{print substr($4,2,length($4)-2)}' )"
     
       device="${part%%[0-9]*}"
+      devpref="${#device[@]}"
+      partn="${part:$devpref}"
     
-      sfdisk --delete $part
+      echo "sfdisk --delete $device $partn"
+      sfdisk --delete $device $partn
     
       partprobe $device
     fi
+
+    echo "Will delete VM"
+    vboxmanage unregistervm $vm_name --delete
+    
     
     
     echo "Will delete VM folder"
