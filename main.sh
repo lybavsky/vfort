e7a5bda27fd8ca9691c0269c26f94383df621b73
a.shiryaev <a.shiryaev@siberian.capital>
2023-09-01 16:31:23 +0400

    fix

--- main.sh
+++ main.sh
@@ -7,8 +7,13 @@ source ${CDIR}/vars.sh
 source ${CDIR}/functions.sh
 
 #If not root then exit
-[ "$EUID" -ne 0 ] && err "Script sould be started as root"
+#TODO: Enable root check back
+# [ "$EUID" -ne 0 ] && err "Script sould be started as root"
 
 if [ $# == 1 ]; then
   echo "Passed parameter: $1"
+
+  [ ! -f "$1" ] && err "File $1 not exists. If you pass param, it should be path to yaml file"
+
+
 fi
